76317f7b73c177d181ab497c5736e64fb1cce70d
Mzkr <alexissourbet@gmail.com>
2015-10-20 00:53:48 +0200

    allow access

--- app/scripts/services/GameService.js
+++ app/scripts/services/GameService.js
@@ -7,27 +7,33 @@
  * # GameService
  */
 angular.module('GameSwap')
-  // use factory for services
-  .factory('GameService', ['$http', function($http) {
+// use factory for services
+.factory('GameService', ['$http',
+  function($http) {
 
     return {
-        doApiCall: function(url, options, payload, headers){
+      doApiCall: function(url, options, cb, headers) {
 
-            // I should make a call to server with the parameters passed
-            // from the controller.
-            var path = options ? "&format=json&"+options : "&format=json";
+        // I should make a call to server with the parameters passed
+        // from the controller.
+        var path = options ? "&format=json&" + options : "&format=json";
+        var APIkey = '626390667861da8f3f8989917b8cd5823146d1c1';
+          
+        $http({
+            method: 'GET',
+            url: "http://www.giantbomb.com/api/" + url + "/?api_key=" + APIkey + path,
+            headers: { 'Access-Control-Allow-Origin': 'http://localhost:9000' }
+        }).then(function successCallback(response) {
             
-            var xhr = $http({
-                method: 'GET',
-                url: "http://www.giantbomb.com/api/" + url + "/?api_key=626390667861da8f3f8989917b8cd5823146d1c1" + path,
-                headers: headers || {}, // Optional headers
-            });
+            cb(response);
+            
+        }, function errorCallback(response) {
+            
+          console.log(response);
+            
+        });
 
-            // You probably want to differentiate success / error handlers
-            xhr.success(payload);
-            xhr.error(payload);
-
-            return xhr;
-        }
+      }
     };
-}]);
+  }
+]);
