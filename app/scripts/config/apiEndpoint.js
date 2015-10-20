c9a4a7843f8ab58946c2afc4e0b9a141f171d6b1
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-10-20 19:19:11 +0200

    merged

--- app/scripts/config/apiEndpoint.js
+++ app/scripts/config/apiEndpoint.js
@@ -14,8 +14,8 @@ angular.module('GameSwap')
 
   // development
   .constant('API_ENDPOINT', {
-    host: 'http://localhost',
-    port: 5000,
+    host: 'https://localhost',
+    port: 55555,
     path: '/api/v1',
     needsAuth: false
   });
--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -10,12 +10,24 @@ angular.module('GameSwap')
   .controller('HomeController', function($scope, ExampleService, UserService, $cordovaOauth) {
     this.users = [];
 
-
-    $cordovaOauth.facebook("392617384261537", ["email"]).then(function(result) {
+    console.log($cordovaOauth);
+    $cordovaOauth.facebook("392617384261537", ["email"], {redirect_uri: "http://localhost/callback"}).then(function(result) {
     	console.log("Result => "+result);
     }, function(error) {	
         console.error('EPIC FAIL : ', error);
     });
+
+    // $scope.googleLogin = function() {
+    //     console.log('tryin to log in to google');
+    //     $cordovaOauth.google("654202242617-vnnhgv43m1nt29b5jakkk8jrp923g8qm.apps.googleusercontent.com", ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
+    //         console.log(JSON.stringify(result));
+    //     }, function(error) {
+    //         console.log(error);
+    //     });
+    // };
+
+    // $scope.googleLogin();
+
 
 
     this.getAllUsers = function() {
