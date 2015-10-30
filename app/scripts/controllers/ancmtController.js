389f458ae02fa6c13ced36fb2274dd06d69d260a
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-10-30 17:34:56 +0100

    list db ancmts ok

--- app/scripts/controllers/ancmtController.js
+++ app/scripts/controllers/ancmtController.js
@@ -8,24 +8,11 @@
  */
 angular.module('GameSwap')
   .controller('AncmtController', function($scope, UserService, AncmtService, GameService, $q) {
-    this.users = [];
-    this.game = [];
-    this.ancmts = [];
+    var self= this;
+    self.ancmts = [];
 
     AncmtService.query().$promise.then(function(data) {
-      console.log(data);
+      self.ancmts = data;
     });
 
-    this.getAllAncmts = function() {
-      //AncmtService.query().$promise.then(function(ancmt) {
-       // console.log(ancmt);
-          /* a supprimer */
-          var ancmt = [{title : "échange gta V", img : "http://i.huffpost.com/gen/1066633/images/o-GTA-5-facebook.jpg"},{title : "échange mgs", img : "http://i.jeuxactus.com/datas/jeux/m/e/metal-gear-solid-5-the-phantom-pain/xl/metal-gear-solid-5-the-54f71952e6bbd.jpg"}];
-          /* */
-          this.ancmts = ancmt;
-
-      //});
-    }
-
-    this.getAllAncmts();
   });
