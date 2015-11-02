a78137f22389e0668d3217fb06bcf17d2f8be58c
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-11-02 19:11:48 +0100

    login mobile ok

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -7,47 +7,79 @@
  * # HomeController
  */
 angular.module('GameSwap')
-  .controller('LoginController', function($cordovaOauth, $window, $http, ServerService, $location, $state) {
-    if(ServerService.isLogged()) {
-        $state.go('app.home');
-    }
+    .controller('LoginController', function($cordovaOauth, $window, $http, ServerService, $location, $state, $q) {
+        if (ServerService.isLogged()) {
+            $state.go('app.home');
+        }
 
-    this.newUser = {};
+        this.newUser = {};
+        var self = this;
 
-    this.loginFacebook = function(){
-        $cordovaOauth.facebook("392617384261537", ["email", "user_friends"]).then(function(result) {
-            $window.localStorage.accessToken = result.access_token;
-            $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $window.localStorage.accessToken, fields: "email,friends,picture", format: "json" }}).then(function(result) {
-                console.log(result.data);
-                ServerService.logUser(result.data.email);
-                $state.go('app/home'); 
+        this.loginFacebook = function() {
+            $cordovaOauth.facebook("392617384261537", ["email", "public_profile"]).then(function(result) {
+                $window.localStorage.accessToken = result.access_token;
+
+                self.getFacebookUserInfos().then(function(user) {
+                    ServerService.logUser(user.email).then(function() {
+                        $state.go('app.home');
+                    }, function(error){
+                        // User not yet created, registering
+                        var newUser = {
+                            name: {
+                                first: user.first_name,
+                                last: user.last_name
+                            },
+                            email: user.email
+                        };
+                        console.log(newUser);
+                        ServerService.registerUser(newUser).then(function() {
+                            $state.go('app.home');
+                        }).catch(function(error) {
+                            console.error('oops', error);
+                        });
+                    });
+
+                });
             }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
-                console.log(error);
-            });
-
-        }, function(error) {    
-            alert('Cannot authenticate in web browser! Faking login');
-            ServerService
-                .logUser("clementpeyrabere@gmail.com")
-                    .then(function(){
-                        $state.go('app.home');            
+                alert('Cannot authenticate in web browser! Faking login');
+                ServerService
+                    .logUser("clementpeyrabere@gmail.com")
+                    .then(function() {
+                        $state.go('app.home');
                     }).catch(function(error) {
                         console.error('oops', error);
                     });
-            
-        });
-    };
 
-    this.register = function() {
-        if(this.user.name.first && this.user.name.last && this.user.email) {
-            ServerService
-                .registerUser(this.user)
-                    .then(function(){
+            });
+        };
+
+        self.getFacebookUserInfos = function() {
+            var deferred = $q.defer();
+            $http.get("https://graph.facebook.com/v2.2/me", {
+                params: {
+                    access_token: $window.localStorage.accessToken,
+                    fields: "first_name,last_name,email",
+                    format: "json"
+                }
+            }).then(function(result) {
+                console.log(result);
+                deferred.resolve(result.data);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        this.register = function() {
+            if (this.user.name.first && this.user.name.last && this.user.email) {
+                ServerService
+                    .registerUser(this.user)
+                    .then(function() {
                         $state.go('app.home');
                     }).catch(function(error) {
                         console.error('oops', error);
                     })
+            }
         }
-    }
-  });
+    });
