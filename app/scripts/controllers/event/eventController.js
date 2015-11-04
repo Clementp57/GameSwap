ca83649ff21f645401451963cb45134cea93b8ca
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-11-04 20:11:39 +0100

    button map in top bar in event list

--- app/scripts/controllers/event/eventController.js
+++ app/scripts/controllers/event/eventController.js
@@ -7,12 +7,35 @@
  * # EventController
  */
 angular.module('GameSwap')
-    .controller('EventController', function(EventService, ServerService) {
+    .controller('EventController', function(EventService, ServerService, $ionicPopup, $scope) {
         var self = this;
         self.events = [];
+        self.eventsCoords = null;
+
+        self.showEventsMap = function(){
+            // An elaborate, custom popup
+            var myPopup = $ionicPopup.show({
+                template: '<map autolocate="true" events="ctrl.events"></map>',
+                title: "Evenements sur la map",
+                cssClass: 'mapPopup',
+                // subTitle: 'Please use normal things',
+                scope: $scope,
+                buttons: [{
+                    text: 'Fermer'
+                }]
+            });
+            myPopup.then(function(res) {
+                console.log('Closed!', res);
+            });
+        };
+
 
         EventService.query().$promise.then(function(events)  {
             self.events = events;
+            self.eventsCoords = [];
+            angular.forEach(self.events, function(event) {
+                self.eventsCoords.push({lat: event.coords.lat, lon: event.coords.lon});
+            });
 
                 // var event = self.events[0];
                 // EventService.addComment({
--- app/scripts/directives/mapDirective.js
+++ app/scripts/directives/mapDirective.js
@@ -1,40 +1,61 @@
 angular.module('GameSwap')
-    .directive('map', function(GeolocationService) {
+    .directive('map', function(GeolocationService, $timeout) {
         return {
             restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
             scope: {
                 //@ reads the attribute value, = provides two-way binding, & works with functions
                 marker: '@',
                 autolocate: '@',
-                description: '@',
-                imageUrl: '@'
+                events: '='
             },
             template: '<div id="map"></div>',
             link: function($scope, element, attrs) {
                 L.mapbox.accessToken = 'pk.eyJ1IjoibXhpbWUiLCJhIjoiNWQ1cDZUcyJ9.SbzQquPm3IbTZluO90hA6A';
-                var map = L.mapbox.map('map').setView([48.855584, 2.354613], 14)
-                        .addLayer(L.mapbox.tileLayer('mapbox.streets'));;
+                var initialize = function() {
+                    var map = L.mapbox.map('map').setView([48.855584, 2.354613], 12)
+                        .addLayer(L.mapbox.tileLayer('mapbox.streets'));
 
-                if(!attrs.autolocate || attrs.autolocate) {
-                    // Localizing user
-                    GeolocationService.getCurrentPosition().then(function(position) {
-                        var lat = position.coords.latitude;
-                        var lon = position.coords.longitude;
-                        addMarkerAndCenter(lat, lon);
+                    if (!attrs.autolocate || attrs.autolocate) {
+                        //Localizing user
+                        GeolocationService.getCurrentPosition().then(function(position) {
+                            var lat = position.coords.latitude;
+                            var lon = position.coords.longitude;
+                            addMarkerAndCenter(lat, lon);
+                        });
+                    }
+
+                    if (attrs.marker && attrs.marker.lat && attrs.marker.lon) {
+                        addMarkerAndCenter(attrs.marker.lat, attrs.marker.lon);
+                    }
+
+                    $scope.$watch('events', function(events) {
+                        angular.forEach(events, function(event, key) {
+                            addMarker(event.coords.lon, event.coords.lat, event.title);
+                        });
                     });
+
+                    function addMarkerAndCenter(lat, lon) {
+                    map.setView([lat, lon], 14);
+                    addMarker(lat, lon);
+                    }
+
+                    function addMarker(lat, lon, content) {
+                        var marker = L.marker([lat, lon]).addTo(map);
+                        if(content) {
+                            marker.bindPopup(content).openPopup();
+                        }
+                    }
                 }
 
-                if(attrs.marker && attrs.marker.lat && attrs.marker.lon) {
-                    addMarkerAndCenter(attrs.marker.lat, attrs.marker.lon);
-                }
+                $timeout(initialize, 500);
+
+                
 
 
 
-                function addMarkerAndCenter(lat, lon) {
-                    map.setView([lat, lon], 16);
-                    L.marker([lat, lon]).addTo(map);
-                }
-               
+                
+
+
             }
         }
     });
