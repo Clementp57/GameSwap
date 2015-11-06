81088131a3cd7cde8cfc5d3e56481c5785ec8738
Mzkr <alexissourbet@gmail.com>
2015-11-06 09:53:06 +0100

    popup confirm delete ancmt

--- app/scripts/controllers/ancmt/myAncmtController.js
+++ app/scripts/controllers/ancmt/myAncmtController.js
@@ -7,26 +7,47 @@
  * # myAncmtController
  */
 angular.module('GameSwap')
-  .controller('MyAncmtController', function(ServerService, AncmtService, $q) {
+  .controller('MyAncmtController', function(ServerService, AncmtService, $q, $ionicPopup) {
     var self = this;
-    self.myAncmts = [];
 
-    if (ServerService.getMyAnnoncement()) {
-        
-      var tblMyAncmt = ServerService.getMyAnnoncement().split(',');
-      for (var i = 0, l = tblMyAncmt.length; i < l; ++i) {
-        AncmtService.get({'id': tblMyAncmt[i]}).$promise.then(function(data) {
-          self.myAncmts.push(data);
-        });
-        // Maybe we can handle this in one request (eg : getAllAncmt for an userId)
+    this.getAllMyAncmt = function() {
+      self.myAncmts = [];
+      if (ServerService.getMyAnnoncement()) {
+        var tblMyAncmt = ServerService.getMyAnnoncement().split(',');
+        for (var i = 0, l = tblMyAncmt.length; i < l; ++i) {
+          AncmtService.get({
+            'id': tblMyAncmt[i]
+          }).$promise.then(function(data) {
+            self.myAncmts.push(data);
+          });
+        }
       }
-    }
-    
-    this.deleteAnnoncement = function (id){
-        AncmtService.delete({'id': id }).$promise.then(function(data) {
-          console.log("Annoncment remove !");
-        });
-        ServerService.removeFromMyAnnoncement(id);
+    };
+
+    this.getAllMyAncmt();
+
+    this.confirmDelete = function(id) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: "Suppression de l'offre",
+        template: 'Êtes vous sur de vouloir supprimer cette offre ?'
+      });
+      confirmPopup.then(function(res) {
+        if (res) {
+          self.deleteAnnoncement(id);
+        } else {
+          return;
+        }
+      });
+    };
+
+    this.deleteAnnoncement = function(id) {
+      AncmtService.delete({
+        'id': id
+      }).$promise.then(function(data) {
+        console.log("Annoncment remove !");
+      });
+      ServerService.removeFromMyAnnoncement(id);
+      this.getAllMyAncmt();
     };
 
   });
